6ad6c5cdf28b954e19b55ce23fca2c7dd0bacf77
GIRESHCHATURVEDI <95335457+GIRESHCHATURVEDI@users.noreply.github.com>
2022-10-18 23:43:13 +0530

    giresh is rockstar

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,23 @@
 package com.driver;
-
+class Product{
+    public int  product(int x , int y){
+        return 0 ;
+    }
+    public int product(int x,int y,int z){
+        return 0 ;
+    }
+    public double product(double x , double y){
+        return 0.0 ;
+    }
+}
 public class Main {
-
-    
-  }
+   public static void main(String [] args){
+       int x = 0 ;
+       int y = 0 ;
+       int z = 0 ;
+     Product make = new Product() ;
+     make.product(x,y) ;
+     make.product(x,y,z) ;
+       int product = make.product(x, y);
+   }
+}
