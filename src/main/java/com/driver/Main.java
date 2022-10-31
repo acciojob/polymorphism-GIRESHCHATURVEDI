76deef3f4405331b2a05e5e019edb5e3bade7bc4
GIRESHCHATURVEDI <95335457+GIRESHCHATURVEDI@users.noreply.github.com>
2022-10-31 18:21:19 +0530

    commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,19 +3,19 @@ package com.driver;
 public class Main {
     static class Product{
     public int product(int x , int y){
-      return x ;
+      return x*y ;
     }
       public int product(int x , int y , int z){
-        return x ;
+        return x*y*z ;
       }
       public double product(double x , double y){
-        return x ;
+        return x*y ;
       }
     }
     public static void main(String[] args){
       Product  p = new Product() ;
-      p.product(10,20) ;
-      p.product(10,20,40) ;
-      p.product(0.2,0.5) ;
+      System.out.println(p.product(10,20)) ;
+      System.out.println(p.product(10,20,40)) ;
+      System.out.println(p.product(0.2,0.5)) ;
     }
   }
