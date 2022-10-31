d897ec50a263c45965b986a852f07427ccd08776
GIRESHCHATURVEDI <95335457+GIRESHCHATURVEDI@users.noreply.github.com>
2022-10-31 16:03:06 +0530

    finakd

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,21 @@
 package com.driver;
 
 public class Main {
-
-    
+    static class Product{
+    public int product(int x , int y){
+      return x ;
+    }
+      public int product(int x , int y , int z){
+        return x ;
+      }
+      public double product(double x , double y){
+        return x ;
+      }
+    }
+    public static void main(String[] args){
+      Product  p = new Product() ;
+      p.product(10,20) ;
+      p.product(10,20,40) ;
+      p.product(0.2,0.5) ;
+    }
   }
